c15e1d152369ea69715313f6b6802ed05eab2b65
Oliver "Aurelius" Ellison <35974550+aurelius-ai@users.noreply.github.com>
2019-08-05 02:57:03 -0500

    13x13-pixel lips in 2x2, 3x3, and 4x4 grids

---
+++ tiny_data/lip2x2_tiny/utils.py
@@ -0,0 +1,113 @@
+import os
+import scipy
+import numpy as np
+import tensorflow as tf
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+def load_mnist(batch_size, is_training=True):
+    path = os.path.join('data', 'mnist')
+    if is_training:
+        fd = open(os.path.join(path, 'train-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainX = loaded[16:].reshape((8400, 26, 26, 1)).astype(np.float32)
+
+        fd = open(os.path.join(path, 'train-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        trainY = loaded[8:].reshape((8400)).astype(np.int32)
+
+        trX = trainX[:7800] / 255.
+        trY = trainY[:7800] 
+
+        valX = trainX[7800:, ] / 255.
+        valY = trainY[7800:]
+
+        num_tr_batch = 7800 // batch_size
+        num_val_batch = 600 // batch_size
+
+        return trX, trY, num_tr_batch, valX, valY, num_val_batch
+    else:
+        fd = open(os.path.join(path, 't10k-images-idx3-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teX = loaded[16:].reshape((600, 26, 26, 1)).astype(np.float)
+
+        fd = open(os.path.join(path, 't10k-labels-idx1-ubyte'))
+        loaded = np.fromfile(file=fd, dtype=np.uint8)
+        teY = loaded[8:].reshape((600)).astype(np.int32)
+
+        num_te_batch = 600 // batch_size
+        return teX / 255., teY, num_te_batch
+
+
+def load_data(dataset, batch_size, is_training=True, one_hot=False):
+    if dataset == 'mnist':
+        return load_mnist(batch_size, is_training)
+    else:
+        raise Exception('Invalid dataset, please check the name of dataset:', dataset)
+
+
+def get_batch_data(dataset, batch_size, num_threads):
+    if dataset == 'mnist':
+        trX, trY, num_tr_batch, valX, valY, num_val_batch = load_mnist(batch_size, is_training=True)
+    elif dataset == 'fashion-mnist':
+        trX, trY, num_tr_batch, valX, valY, num_val_batch = load_fashion_mnist(batch_size, is_training=True)
+    data_queues = tf.train.slice_input_producer([trX, trY])
+    X, Y = tf.train.shuffle_batch(data_queues, num_threads=num_threads,
+                                  batch_size=batch_size,
+                                  capacity=batch_size * 64,
+                                  min_after_dequeue=batch_size * 32,
+                                  allow_smaller_final_batch=False)
+
+    return(X, Y)
+
+
+def save_images(imgs, size, path):
+    '''
+    Args:
+        imgs: [batch_size, image_height, image_width]
+        size: a list with tow int elements, [image_height, image_width]
+        path: the path to save images
+    '''
+    imgs = (imgs + 1.) / 2  # inverse_transform
+    return(scipy.misc.imsave(path, mergeImgs(imgs, size)))
+
+
+def mergeImgs(images, size):
+    h, w = images.shape[1], images.shape[2]
+    imgs = np.zeros((h * size[0], w * size[1], 3))
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx // size[1]
+        imgs[j * h:j * h + h, i * w:i * w + w, :] = image
+
+    return imgs
+
+
+# For version compatibility
+def reduce_sum(input_tensor, axis=None, keepdims=False):
+    try:
+        return tf.reduce_sum(input_tensor, axis=axis, keepdims=keepdims)
+    except:
+        return tf.reduce_sum(input_tensor, axis=axis, keep_dims=keepdims)
+
+
+# For version compatibility
+def softmax(logits, axis=None):
+    try:
+        return tf.nn.softmax(logits, axis=axis)
+    except:
+        return tf.nn.softmax(logits, dim=axis)
+
+
+def get_shape(inputs, name=None):
+    name = "shape" if name is None else name
+    with tf.name_scope(name):
+        static_shape = inputs.get_shape().as_list()
+        dynamic_shape = tf.shape(inputs)
+        shape = []
+        for i, dim in enumerate(static_shape):
+            dim = dim if dim is not None else dynamic_shape[i]
+            shape.append(dim)
+        return(shape)
